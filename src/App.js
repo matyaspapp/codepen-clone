78553419049c367b16a8266f729a5794650324a0
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-20 21:44:51 +0200

    add editor component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Editor from './Components/Editor';
 
 function App() {
   return (
     <div className="app">
-      
+      <div className="app__editors">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="app__frame">
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Editor/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const Editor = () => {
+  return (
+    <div className="editor">
+      <h1>Editor</h1>
+    </div>
+  );
+};
+
+export default Editor;
