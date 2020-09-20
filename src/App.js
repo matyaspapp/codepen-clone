b6f11cf409da4b1a666e20bdf70e4ffd745a6064
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-20 22:17:33 +0200

    add editor functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Editor from './Components/Editor';
 
 function App() {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+
   return (
     <div className="app">
       <div className="app__editors">
-        <Editor />
-        <Editor />
-        <Editor />
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className="app__frame">
 
--- src/Components/Editor/index.js
+++ src/Components/Editor/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './index.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
 
-const Editor = () => {
+
+const Editor = ({ displayName, language, value, onChange }) => {
+
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+
   return (
     <div className="editor">
-      <h1>Editor</h1>
+      <div className="editor__header">
+        {displayName}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        className="controlledEditor__wrapper"
+        onBeforeChange={handleChange}
+        value={value}
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true,
+        }}
+      />
     </div>
   );
 };
